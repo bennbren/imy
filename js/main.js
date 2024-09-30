cae0053d90b2e8ee2344608075de1226aad2d877
root <root@localhost>
2024-10-01 00:36:02 +0800

    auto publish 1727714162239

--- js/main.js
+++ js/main.js
@@ -8,3 +8,14 @@ window.addEventListener('load', () => {
         clearInterval(interval);
     }, 5000);
 });
+
+
+document.getElementById('openYouTube').addEventListener('click', function() {
+    const videoUrl = 'https://youtu.be/eFwQIxlFTXU?si=YrCitFkrGI9GZUt5';
+    window.open(videoUrl, '_blank'); // Opens the link in a new tab
+});
+
+document.getElementById('openYouTube2').addEventListener('click', function() {
+    const videoUrl = 'https://youtube.com/shorts/5JhI02EbQmg?si=YB1tER6Ja4XPjI67';
+    window.open(videoUrl, '_blank'); // Opens the link in a new tab
+});
